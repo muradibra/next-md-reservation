7c96fa41d3665282b29edded01bd759ed66f76ca
muradibra <muradibrahimov495@gmail.com>
2024-11-14 18:14:35 +0400

    feat: contact us page created

---
+++ app/(business)/contact/page.tsx
@@ -0,0 +1,12 @@
+import { ContactUs } from "@/components/shared/ContactUs";
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <ContactUs />
+    </div>
+  );
+};
+
+export default ContactPage;
